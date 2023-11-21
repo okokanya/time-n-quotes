2b9a645ca9dbfd5f5093ed0bf55b97c3e993b932
anya pushkina <okokanya@gmail.com>
2023-11-21 10:26:36 +0800

    добавила цитаты

--- quotes.js
+++ quotes.js
@@ -2,95 +2,118 @@ var quotes = {
   am1: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am2: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am3: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am4: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am5: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am6: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am7: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am8: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am9: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am10: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am11: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   am12: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
 
   pm1: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm2: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm3: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm4: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm5: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm6: {
-    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
-    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    quote: 'На часах уж шесть было, а его всё нет. У меня уж и сердце не на месте: не случилось ли чего?',
+    author: 'Александр Сергеевич Пушкин',
+    sourse: 'Капитанская дочка'
   },
   pm7: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm8: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm9: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm10: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
     author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    sourse: ''
   },
   pm11: {
-    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
-    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+    quote: 'Раз пробило уже одиннадцать часов, и пора было идти домой, но всё ещё не хотелось расставаться с таким весёлым обществом',
+    author: 'Фёдор Михайлович Достоевский',
+    sourse: 'Преступление и наказание'
   },
   pm12: {
     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
