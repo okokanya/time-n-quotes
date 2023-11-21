d02f90ac4d92a1ca5edf5073ef8b51a25159cad9
anya pushkina <okokanya@gmail.com>
2023-11-21 09:52:21 +0800

    добавила файл с цитатами

---
+++ quotes.js
@@ -0,0 +1,99 @@
+var quotes = {
+  am1: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am2: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am3: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am4: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am5: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am6: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am7: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am8: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am9: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am10: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am11: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  am12: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+
+  pm1: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm2: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm3: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm4: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm5: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm6: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm7: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm8: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm9: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm10: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm11: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  },
+  pm12: {
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla ',
+    author: 'dui sit amet purus dapibus, eget suscipit elit aliquam',
+  }
+};
