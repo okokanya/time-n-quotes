82dd3483601907693bf6763139a10b2ff2baceca
Mikhail Zakharov <erreal@yandex.ru>
2023-12-05 15:32:50 +0300

    quotes and functions to send them

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 import express from "express";
 import path from 'path';
 import {fileURLToPath} from 'url';
+import { sendQuote } from "./srv/quotesApi.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/time', (req, resp) => {
-    resp.status(200).send({
-        cit: "Lorem",
-        time: 0
-    });
+app.get('/api', (req, resp) => {
+    resp.status(200).send(sendQuote(req.query?.time));
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next){
+    res.status(404);
+    res.send({ error: 'Not found' });
+    return;
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.send({ error: err.message });
+    return;
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
